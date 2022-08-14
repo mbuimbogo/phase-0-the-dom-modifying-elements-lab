d4ebab26e3ed4bba1b68130d2bd404e77de9fc33
“mbuimbogo” <peter.mbogo@student.moringaschool.com>
2022-08-14 15:26:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+document.querySelector("main");
+main.remove ()
+
+
+var newHeader = document.createElement('h1');
+// document.body.append(newHeader);
+newHeader.id = 'victory';
+
+
+newHeader.innerHTML = "YOUR-NAME is the champion";
